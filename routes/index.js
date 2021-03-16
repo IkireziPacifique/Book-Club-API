afc81d9759aa5e1594fd3ed85851e0b0bbc127d8
Linda Pacifique <71096252+IkireziPacifique@users.noreply.github.com>
2021-03-16 23:31:38 +0200

    Book Club API Implementation

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 import express from "express";
 import membersRouter from "./membersRouter.js"
-import usersRouter from './usersRouter.js'
+import booksRouter from './booksRouter.js';
+import borrowRouter from "./borrowRouter.js";
 
 const router = express.Router();
 
 
+
 router.use("/members", membersRouter);
-router.use("/users", usersRouter);
+router.use("/books", booksRouter);
+router.use("/borrows", borrowRouter);
+
 
 export default router;
